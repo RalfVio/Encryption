5e9aa2c6dd5038074a33200d1974bf2953e75ebb
Ralf Viola <Ralf.Viola@philips.com>
2022-07-21 10:12:53 -0400

    Initial Commit

---
+++ Encryption/StartForm.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace Encryption
+{
+    partial class StartForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.encryptToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.decryptToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.encryptionKeyTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.addBrTagsCheckBox = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(158, 34);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(158, 34);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.CloseToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.encryptToolStripMenuItem1,
+            this.decryptToolStripMenuItem1});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(58, 29);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // encryptToolStripMenuItem1
+            // 
+            this.encryptToolStripMenuItem1.Name = "encryptToolStripMenuItem1";
+            this.encryptToolStripMenuItem1.Size = new System.Drawing.Size(270, 34);
+            this.encryptToolStripMenuItem1.Text = "Encrypt";
+            this.encryptToolStripMenuItem1.Click += new System.EventHandler(this.EncryptToolStripMenuItem_Click);
+            // 
+            // decryptToolStripMenuItem1
+            // 
+            this.decryptToolStripMenuItem1.Name = "decryptToolStripMenuItem1";
+            this.decryptToolStripMenuItem1.Size = new System.Drawing.Size(270, 34);
+            this.decryptToolStripMenuItem1.Text = "Decrypt";
+            this.decryptToolStripMenuItem1.Click += new System.EventHandler(this.DecryptToolStripMenuItem_Click);
+            // 
+            // encryptionKeyTextBox
+            // 
+            this.encryptionKeyTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.encryptionKeyTextBox.Location = new System.Drawing.Point(108, 74);
+            this.encryptionKeyTextBox.Name = "encryptionKeyTextBox";
+            this.encryptionKeyTextBox.Size = new System.Drawing.Size(659, 31);
+            this.encryptionKeyTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Key:";
+            // 
+            // dataTextBox
+            // 
+            this.dataTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataTextBox.Location = new System.Drawing.Point(28, 157);
+            this.dataTextBox.Multiline = true;
+            this.dataTextBox.Name = "dataTextBox";
+            this.dataTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataTextBox.Size = new System.Drawing.Size(739, 399);
+            this.dataTextBox.TabIndex = 3;
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "txt";
+            // 
+            // addBrTagsCheckBox
+            // 
+            this.addBrTagsCheckBox.AutoSize = true;
+            this.addBrTagsCheckBox.Location = new System.Drawing.Point(108, 114);
+            this.addBrTagsCheckBox.Name = "addBrTagsCheckBox";
+            this.addBrTagsCheckBox.Size = new System.Drawing.Size(142, 29);
+            this.addBrTagsCheckBox.TabIndex = 5;
+            this.addBrTagsCheckBox.Text = "Add BR tags ";
+            this.addBrTagsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 580);
+            this.Controls.Add(this.addBrTagsCheckBox);
+            this.Controls.Add(this.dataTextBox);
+            this.Controls.Add(this.encryptionKeyTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "StartForm";
+            this.Text = "Encryption Tool";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.TextBox encryptionKeyTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox dataTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem encryptToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem decryptToolStripMenuItem1;
+        private System.Windows.Forms.CheckBox addBrTagsCheckBox;
+    }
+}
+
---
+++ Encryption/StartForm.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace Encryption
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.encryptionKeyTextBox.Text))
+                return;
+
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            var lines= File.ReadAllLines(openFileDialog.FileName);
+            var key = GetKey();
+            this.dataTextBox.Text = Decrypt(lines, key);
+        }
+        private void EncryptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.dataTextBox.Text) || string.IsNullOrEmpty(this.encryptionKeyTextBox.Text))
+                return;
+
+            var key = GetKey();
+            var convertedText = Encrypt(this.dataTextBox.Lines, key, this.addBrTagsCheckBox.Checked);
+            this.dataTextBox.Text = convertedText;
+        }
+        private void DecryptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.dataTextBox.Text)|| string.IsNullOrEmpty(this.encryptionKeyTextBox.Text))
+                return;
+
+            var key = GetKey();
+            var convertedText=Decrypt(this.dataTextBox.Lines, key);
+            this.dataTextBox.Text = convertedText;
+        }
+
+        byte[] GetKey() => GetHash(this.encryptionKeyTextBox.Text);
+        static byte[] GetHash(string text)
+        {
+            byte[] result = new byte[128 / 8];
+
+            using (var sha256 = SHA256.Create())
+            {
+                result = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+            }
+
+            return result;
+        }
+
+        static string Encrypt(string[] lines, byte[] key, bool addBrTags)
+        {
+            StringBuilder sb = null;
+            try
+            {
+                byte[] result = null;
+
+                using (var ms = new MemoryStream())
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = key;
+
+                    byte[] iv = aes.IV;
+                    ms.Write(iv, 0, iv.Length);
+
+                    using (var cryptoStream = new CryptoStream(
+                        ms,
+                        aes.CreateEncryptor(),
+                        CryptoStreamMode.Write))
+                    {
+                        using (var encryptWriter = new StreamWriter(cryptoStream))
+                        {
+                            foreach (var line in lines)
+                                encryptWriter.WriteLine(line);
+                        }
+                    }
+                    result = ms.ToArray();
+                }
+                sb = new StringBuilder();
+                int i = 0;
+                foreach (var b in result)
+                {
+                    i++;
+                    if (i % 25 == 1 & i > 1)
+                        sb.Append("\r\n" + (addBrTags ? "<br/>" : ""));
+                    sb.Append(b.ToString("x2"));
+                }
+
+            }
+            catch
+            {
+                throw;
+            }
+
+            if (sb == null)
+                return "";
+            else
+                return sb.ToString();
+        }
+
+        static string Decrypt(string[] lines, byte[] key)
+        {
+            string result = null;
+            try
+            {
+                var ms = new MemoryStream();
+                foreach (var line in lines)
+                {
+                    for (int i = (line.StartsWith("<br/>") ? 5 : 0); i < line.Length; i += 2)
+                    {
+                        ms.WriteByte(Convert.ToByte(line.Substring(i, 2), 16));
+                    }
+                }
+
+                ms.Position = 0;
+
+                using (Aes aes = Aes.Create())
+                {
+                    byte[] iv = new byte[aes.IV.Length];
+                    int numBytesToRead = aes.IV.Length;
+                    int numBytesRead = 0;
+                    while (numBytesToRead > 0)
+                    {
+                        int n = ms.Read(iv, numBytesRead, numBytesToRead);
+                        if (n == 0) break;
+
+                        numBytesRead += n;
+                        numBytesToRead -= n;
+                    }
+
+                    using (var cryptoStream = new CryptoStream(
+                       ms,
+                       aes.CreateDecryptor(key, iv),
+                       CryptoStreamMode.Read))
+                    {
+                        using (var decryptReader = new StreamReader(cryptoStream))
+                        {
+                            string decryptedMessage = decryptReader.ReadToEnd();
+                            result = decryptedMessage;
+                        }
+                    }
+
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            return result ?? "";
+        }
+
+
+
+    }
+}
